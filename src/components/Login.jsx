ebb1070476029055682446c40538a98555dcdf22
Diizzii <dieter@kribbel.at>
2021-06-25 13:30:10 +0200

    25061330

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,6 @@
 import React from 'react'
-import * as Yup from 'yup'
 
-const defaultValues = {
-  email: '',
-  password: '',
-}
-
-const validationSchema = Yup.object().shape({
-  email: Yup.string().required('Please enter your email address!'),
-  password: Yup.string().required('Please enter your password!'),
-})
+import { defaultValues, validationSchema } from '../service/loginSetup'
 
 const Login = () => {
   return <div></div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as Yup from 'yup'
+import { Formik, Form } from 'formik'
+import { useHistory } from 'react-router-dom'
+
+import { defaultValues, validationSchema } from '../service/signupSetup'
 
 const Signup = () => {
+  const history = useHistory()
+  const [serverError, setServerError] = useState('')
+
   return <div></div>
 }
 
